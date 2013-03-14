b28fb1fcfafb7bffa8c5a40d88091f71c771985c
smike <smike@google.com>
2013-03-14 02:46:26 -0400

    adding html page to test api calls and some cleanup.

--- app.js
+++ app.js
@@ -4,8 +4,6 @@
  */
 
 var express = require('express')
-  , routes = require('./routes')
-  , user = require('./routes/user')
   , api = require('./routes/api')
   , http = require('http')
   , path = require('path');
@@ -21,18 +19,15 @@ app.configure(function(){
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(app.router);
-  app.use(express.static(path.join(__dirname, 'public')));
+  app.use('/static', express.static(path.join(__dirname, 'public')));
 });
 
 app.configure('development', function(){
   app.use(express.errorHandler());
 });
 
-//app.get('/', routes.index);
-app.all('/api/content/:method/:arg1?/:arg2?/:arg3?', api.content);
-//app.get('/users', user.list);
-
-console.log(app.routes)
+app.get('/api/content/:method/:arg1?/:arg2?/:arg3?', api.content);
+app.post('/api/content/:method/:arg1?/:arg2?/:arg3?', api.content);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
